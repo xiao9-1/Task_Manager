61378a875793be8451e851c48af046df570edf8d
Kirill <matveeffew@gmail.com>
2026-04-22 16:09:21 +0300

    новый метод getUserTasks

--- src/main/java/com/example/task_manager/controller/TaskController.java
+++ src/main/java/com/example/task_manager/controller/TaskController.java
@@ -33,7 +33,7 @@ public class TaskController {
     public List<TaskResponse> getAllTasks() {
         log.info("GET /tasks - получение всех задач");
         return taskService.getAllTasks().stream()
-                .map(TaskResponse::new)
+                .map(TaskResponse::from)
                 .toList();
     }
 
@@ -42,23 +42,23 @@ public class TaskController {
     public TaskResponse getTaskById(@PathVariable("id") Long id) {
         log.info("GET /tasks/{} - запрос задачи по ID", id);
         Task task = taskService.getTaskById(id);
-        return new TaskResponse(task);
+        return TaskResponse.from(task);
     }
 
     // POST /tasks - создать задачу
     @PostMapping
     public ResponseEntity<TaskResponse> createTask(@RequestBody TaskRequest request) {
         log.info("POST /tasks - запрос на создание задачи: title='{}', dueTime={}",
-                request.getTitle(), request.getDueTime());
+                request.title(), request.dueTime());
         Task newTask = taskService.createTask(request);
         log.info("POST /tasks - задача создана с ID={}", newTask.getId());
-        return ResponseEntity.status(HttpStatus.CREATED).body(new TaskResponse(newTask));
+        return ResponseEntity.status(HttpStatus.CREATED).body(TaskResponse.from(newTask));
     }
 
     @PostMapping("/{id}/complete")
     public TaskResponse completeTask(@PathVariable("id") Long id) {
         Task task = taskService.completeTask(id);
-        return new TaskResponse(task);
+        return TaskResponse.from(task);
     }
 
     // PUT /tasks/{id} - обновить задачу
@@ -72,7 +72,7 @@ public class TaskController {
             return ResponseEntity.notFound().build();
         }
         
-        return ResponseEntity.ok(new TaskResponse(updatedTask));
+        return ResponseEntity.ok(TaskResponse.from(updatedTask));
     }
 
     // DELETE /tasks/{id} - удалить задачу
@@ -88,6 +88,15 @@ public class TaskController {
 
         log.info("DELETE /tasks/{} - задача удалена", id);
         return ResponseEntity.noContent().build();
+    }
+
+    // GET /tasks/user/{userId} - получить задачи конкретного пользователя
+    @GetMapping("/user/{userId}")
+    public List<TaskResponse> getUserTasks(@PathVariable("userId") Long userId) {
+        log.info("GET /tasks/user/{} - получение задач пользователя", userId);
+        return taskService.getTasksByUserId(userId).stream()
+                .map(TaskResponse::from)
+                .toList();
     }
 
 }
