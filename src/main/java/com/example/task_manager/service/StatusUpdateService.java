86d2772336947709b1600900f86b53f5f6fa3c88
Kirill <matveeffew@gmail.com>
2026-04-23 11:42:30 +0300

    убраны лишние импорты

--- src/main/java/com/example/task_manager/service/StatusUpdateService.java
+++ src/main/java/com/example/task_manager/service/StatusUpdateService.java
@@ -9,9 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 @EnableScheduling
