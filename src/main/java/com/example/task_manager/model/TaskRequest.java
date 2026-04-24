c06fa3c4e20b9e9058d34b0c141da83363a129d8
Kirill <matveeffew@gmail.com>
2026-04-24 15:53:34 +0300

    вынесены в dto

--- src/main/java/com/example/task_manager/model/TaskRequest.java
+++
@@ -1,6 +0,0 @@
-package com.example.task_manager.model;
-
-import java.time.LocalDateTime;
-
-public record TaskRequest(String title, LocalDateTime dueTime, Long userId, Double rating) {
-}
--- src/main/java/com/example/task_manager/model/UserRequest.java
+++
@@ -1,5 +0,0 @@
-package com.example.task_manager.model;
-
-public record  UserRequest(String name, String email) {
-};
-
