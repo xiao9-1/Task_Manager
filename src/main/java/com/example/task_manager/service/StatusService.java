6a87ca552efce7a3d75e2263044fd957360e9eee
Kirill <matveeffew@gmail.com>
2026-04-28 16:05:40 +0300

    добавлены логи

--- src/main/java/com/example/task_manager/service/StatusService.java
+++ src/main/java/com/example/task_manager/service/StatusService.java
@@ -3,23 +3,45 @@ package com.example.task_manager.service;
 import com.example.task_manager.model.Status;
 import com.example.task_manager.model.Task;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 
 @Service
 public class StatusService {
 
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
+
     public Status getCurrentStatus(Task task) {
+
+        log.debug("Расчёт статуса задачи: ID={}, dueTime={}, completedAt={}",
+                task.getId(), task.getDueTime(), task.getCompletedAt());
+
         LocalDateTime now = LocalDateTime.now();
+
         if (task.getCompletedAt() != null) {
-            return task.getCompletedAt().isBefore(task.getDueTime())
-                ? Status.COMPLETED_ON_TIME
-                : Status.COMPLETED_LATE;
+
+            Status status = task.getCompletedAt().isBefore(task.getDueTime())
+                    ? Status.COMPLETED_ON_TIME
+                    : Status.COMPLETED_LATE;
+
+            log.info("Задача ID={} завершена. Итоговый статус={}",
+                    task.getId(), status);
+
+            return status;
         }
-        else {
-            return now.isAfter(task.getDueTime())
-            ? Status.NOT_COMPLETED
-            : Status.PENDING;
+
+        if (now.isAfter(task.getDueTime())) {
+
+            log.info("Задача ID={} просрочена. Устанавливается статус NOT_COMPLETED",
+                    task.getId());
+
+            return Status.NOT_COMPLETED;
         }
+
+        log.debug("Задача ID={} ещё не просрочена. Статус=PENDING", task.getId());
+
+        return Status.PENDING;
     }
 }
