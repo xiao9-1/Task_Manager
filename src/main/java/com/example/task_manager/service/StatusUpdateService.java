6034bdb790a46ff3236982c041e845cfab2ba70b
Kirill <matveeffew@gmail.com>
2026-04-28 16:16:58 +0300

    добавлены логи, убран мап, вынесен шедулер( запускает updateAllStatuses)

--- src/main/java/com/example/task_manager/service/StatusUpdateService.java
+++ src/main/java/com/example/task_manager/service/StatusUpdateService.java
@@ -4,52 +4,48 @@ import com.example.task_manager.model.Task;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
+import java.util.Collection;
 
 @Service
-@EnableScheduling
 public class StatusUpdateService {
 
-    private static final Logger log = LoggerFactory.getLogger(StatusUpdateService.class);
-    
-    private final Map<Long, Task> tasks;
-    private final StatusService statusService;
-    
-    private final TaskService taskService;
-    
-    public StatusUpdateService(TaskService taskService, StatusService statusService) {
-        this.taskService = taskService;
-        this.statusService = statusService;
-        this.tasks = taskService.getTasksMap();
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
 
+    private final StatusService statusService;
+
+    public StatusUpdateService(StatusService statusService) {
+        this.statusService = statusService;
     }
-     
-    @Scheduled(fixedDelay = 10000)
-    public void updateAllStatuses() {
-        log.info("Фоновое обновление статусов задач...");
-        
-        int updatedCount = 0;
-        
-        for (Task task : tasks.values()) {
-            Status oldStatus = task.getStatus();
-            Status newStatus = statusService.getCurrentStatus(task);
-            
-            if (oldStatus != newStatus) {
-                task.setStatus(newStatus);
-                updatedCount++;
-                log.info("Задача ID={}: статус изменён с {} на {}", 
+
+    public int updateAllStatuses(Collection<Task> tasks) {
+
+    log.debug("Запуск обновления статусов. Количество задач: {}", tasks.size());
+
+    int updated = 0;
+
+    for (Task task : tasks) {
+
+        Status oldStatus = task.getStatus();
+        Status newStatus = statusService.getCurrentStatus(task);
+
+        if (oldStatus != newStatus) {
+
+            task.setStatus(newStatus);
+            updated++;
+
+            log.info("Задача ID={} обновлена: {} -> {}",
                     task.getId(), oldStatus, newStatus);
-            }
         }
-        
-        if (updatedCount > 0) {
-            log.info("Обновлено {} задач", updatedCount);
-        } else {
-            log.debug("Статусы актуальны, изменений нет");
+        else {
+            log.debug("Задача ID={} без изменений: статус={}",
+                    task.getId(), oldStatus);
         }
-    } 
+    }
+
+    log.info("Обновление статусов завершено. Изменено задач: {}", updated);
+
+    return updated;
+}
 }
