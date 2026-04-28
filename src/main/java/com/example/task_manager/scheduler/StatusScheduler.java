1f2ceed3ce6701ea73166c4304385d244651f875
Kirill <matveeffew@gmail.com>
2026-04-28 16:05:08 +0300

    шедулер вынесен в отдельный слой

---
+++ src/main/java/com/example/task_manager/scheduler/StatusScheduler.java
@@ -0,0 +1,27 @@
+package com.example.task_manager.scheduler;
+
+import com.example.task_manager.repository.TaskRepository;
+import com.example.task_manager.service.StatusUpdateService;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StatusScheduler {
+
+    private final StatusUpdateService statusUpdateService;
+    private final TaskRepository taskRepository;
+
+    public StatusScheduler(StatusUpdateService statusUpdateService,
+                           TaskRepository taskRepository) {
+        this.statusUpdateService = statusUpdateService;
+        this.taskRepository = taskRepository;
+    }
+
+    @Scheduled(fixedDelay = 10000)
+    public void run() {
+        statusUpdateService.updateAllStatuses(
+            taskRepository.findAll()
+        );
+    }
+}
