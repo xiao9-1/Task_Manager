a20e3d587298268d4c3979d87500cea469beb1ec
Kirill <matveeffew@gmail.com>
2026-05-12 15:54:13 +0300

    убрана старая реализация

--- src/main/java/com/example/task_manager/repository/TaskRepository.java
+++ src/main/java/com/example/task_manager/repository/TaskRepository.java
@@ -8,15 +8,6 @@ import org.springframework.stereotype.Repository;
 
 import com.example.task_manager.model.Task;
 
-// public interface TaskRepository {
-//     Task save(Task task);
-//     Optional<Task> findById(Long id);
-//     List<Task> findAll();
-//     List<Task> findAllByUserId(Long userId);
-//     void deleteById(Long id);
-//     boolean existsById(Long id);
-// }
-
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findAllByUserId(Long userId);
