cff3eb84070730aff398af444161805cd2b3bf6f
Kirill <matveeffew@gmail.com>
2026-05-12 11:17:00 +0300

    sql src for db

---
+++ src_db_init/create_ddl.sql
@@ -0,0 +1,19 @@
+create table users (
+    id bigserial primary key,
+    name varchar(255) not null,
+    email varchar(255) unique not null,
+    created_at timestamp,
+    task_count int,
+    is_top boolean
+);
+
+create table tasks (
+    id bigserial primary key,
+    title varchar(255),
+    status varchar(50),
+    created_at timestamp,
+    due_time timestamp,
+    completed_at timestamp,
+    rating double precision,
+    user_id bigint references users(id)
+);
