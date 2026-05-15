1ff8c96618c27737ad5368857c249291f6f64ee9
Kirill <matveeffew@gmail.com>
2026-05-15 14:32:31 +0300

    service для security

---
+++ src/main/java/com/example/task_manager/security/UserSecurityService.java
@@ -0,0 +1,39 @@
+package com.example.task_manager.security;
+
+import java.util.Collections;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.task_manager.model.User;
+import com.example.task_manager.repository.UserRepository;
+
+@Service
+public class UserSecurityService implements UserDetailsService{
+
+    private final UserRepository userRepository;
+
+    public UserSecurityService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден" + email));
+
+
+        return new org.springframework.security.core.userdetails.User(
+            user.getEmail(),
+            "{noop}" + user.getPassword(),
+            Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole()))
+        );
+        
+    }
+
+    
+    
+}
