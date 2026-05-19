3fe4d4acb6ab336c80160201824debc2123a6f06
Kirill <matveeffew@gmail.com>
2026-05-19 10:40:36 +0300

    added info

--- src/main/java/com/example/task_manager/controller/InfoController.java
+++ src/main/java/com/example/task_manager/controller/InfoController.java
@@ -7,18 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.task_manager.component.InfoService;
+
 @RestController
 @RequestMapping("/info")
 public class InfoController {
 
-    @Value("${app.version}")
-    private String version;
+    private final InfoService infoService;
+
+    public InfoController(InfoService infoService) {
+        this.infoService = infoService;
+    }
 
     @GetMapping("/version")
     public Map<String, String> getVersion() {
-
         return Map.of(
-                "version", version,
+                "version", infoService.getVersion(),
                 "text", "TEXT"
         );
     }
