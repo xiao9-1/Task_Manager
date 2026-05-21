e4ac2358303e02427963cbcd17701cd246030ad0
Kirill <matveeffew@gmail.com>
2026-05-21 10:10:58 +0300

    название приложения в тексте

--- src/main/java/com/example/task_manager/controller/InfoController.java
+++ src/main/java/com/example/task_manager/controller/InfoController.java
@@ -2,7 +2,6 @@ package com.example.task_manager.controller;
 
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,7 +22,7 @@ public class InfoController {
     public Map<String, String> getVersion() {
         return Map.of(
                 "version", infoService.getVersion(),
-                "text", "TEXT"
+                "text", "Task Manager"
         );
     }
 }
