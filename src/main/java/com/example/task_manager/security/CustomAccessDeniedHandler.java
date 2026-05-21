8a43f089b293811434939e4e480adb3578942537
Kirill <matveeffew@gmail.com>
2026-05-21 10:16:06 +0300

    обработчик ошибки доступа

---
+++ src/main/java/com/example/task_manager/security/CustomAccessDeniedHandler.java
@@ -0,0 +1,35 @@
+package com.example.task_manager.security;
+
+import java.io.IOException;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException
+    ) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("application/json");
+
+        response.getWriter().write(
+                """
+                {
+                    "status": 403,
+                    "error": "Forbidden",
+                    "message": "%s"
+                }
+                """.formatted(accessDeniedException.getMessage())
+        );
+    }
+}
