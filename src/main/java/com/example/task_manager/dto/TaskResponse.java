240d70ad746e2b5d67949bed2735c142fbb91c69
Kirill <matveeffew@gmail.com>
2026-05-22 14:28:19 +0300

    поле статуса только типа Status

--- src/main/java/com/example/task_manager/dto/TaskResponse.java
+++ src/main/java/com/example/task_manager/dto/TaskResponse.java
@@ -2,13 +2,14 @@ package com.example.task_manager.dto;
 
 import java.time.LocalDateTime;
 
+import com.example.task_manager.model.Status;
 import com.example.task_manager.model.Task;
 
 public record TaskResponse(
     Long id,
     Long userId, 
     String title, 
-    String status, 
+    Status status, 
     LocalDateTime dueTime, 
     LocalDateTime completedAt,
     Double rating
@@ -19,7 +20,7 @@ public record TaskResponse(
             task.getId(),
             task.getUserId(),
             task.getTitle(),
-            task.getStatus().name(),
+            task.getStatus(),
             task.getDueTime(),
             task.getCompletedAt(),
             task.getRating()
