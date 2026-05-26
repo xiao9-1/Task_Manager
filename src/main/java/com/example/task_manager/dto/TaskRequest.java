af1bba7b9ba000573ea0287284451ae3eecaf037
Kirill <matveeffew@gmail.com>
2026-05-26 15:36:04 +0300

    добавлен айди проекта

--- src/main/java/com/example/task_manager/dto/TaskRequest.java
+++ src/main/java/com/example/task_manager/dto/TaskRequest.java
@@ -2,5 +2,5 @@ package com.example.task_manager.dto;
 
 import java.time.LocalDateTime;
 
-public record TaskRequest(String title, LocalDateTime dueTime, Long userId, Double rating) {
+public record TaskRequest(String title, LocalDateTime dueTime, Long userId, Double rating, Long projectId) {
 }
