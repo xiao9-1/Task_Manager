18c9446c4fcaa6c5e7c8082fd7e02bb61bd63fad
Kirill <matveeffew@gmail.com>
2026-05-26 15:39:19 +0300

    метод для получения количества задач по проекту

--- src/main/java/com/example/task_manager/repository/TaskRepository.java
+++ src/main/java/com/example/task_manager/repository/TaskRepository.java
@@ -4,11 +4,27 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.example.task_manager.dto.UserProjectTaskReport;
 import com.example.task_manager.model.Task;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findAllByUserId(Long userId);
+
+    @Query("""
+    SELECT new com.example.task_manager.dto.UserProjectTaskReport(
+        t.userId,
+        t.project.id,
+        COUNT(t)
+    )
+    FROM Task t
+    WHERE (:userId IS NULL OR t.userId = :userId)
+    GROUP BY t.userId, t.project.id
+    """)
+    List<UserProjectTaskReport> getUserProjectTaskReport(@Param("userId") Long userId);
+
 }
