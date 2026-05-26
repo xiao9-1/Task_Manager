51efc9aa4f801ce2b90290ced8f10ea7018339cd
Kirill <matveeffew@gmail.com>
2026-05-26 15:34:01 +0300

    новый админ контроллер

---
+++ src/main/java/com/example/task_manager/controller/AdminController.java
@@ -0,0 +1,40 @@
+package com.example.task_manager.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.task_manager.dto.UserProjectTaskReport;
+import com.example.task_manager.security.CustomUserDetails;
+import com.example.task_manager.service.TaskService;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    private final TaskService taskService;
+    
+    public AdminController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/report/tasks")
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<UserProjectTaskReport> getReport(
+            @RequestParam(required = false) Long userId,
+            @AuthenticationPrincipal CustomUserDetails user)
+
+        {
+        return taskService.getReport(userId, user.getRole());
+    }
+    
+}
