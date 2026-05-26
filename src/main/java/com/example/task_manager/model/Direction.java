15c4349fe8c7abccb78b67e67cb05ec14447e1bc
Kirill <matveeffew@gmail.com>
2026-05-26 15:36:32 +0300

    сущность направление

---
+++ src/main/java/com/example/task_manager/model/Direction.java
@@ -0,0 +1,40 @@
+package com.example.task_manager.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "directions")
+public class Direction {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "direction")
+    private List<Project> projects;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+  
+}
