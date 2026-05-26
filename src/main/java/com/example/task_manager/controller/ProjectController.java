1f10aab35644882795ce3423563e3ce8ba615f77
Kirill <matveeffew@gmail.com>
2026-05-26 15:35:01 +0300

    контроллер проекта

---
+++ src/main/java/com/example/task_manager/controller/ProjectController.java
@@ -0,0 +1,50 @@
+package com.example.task_manager.controller;
+
+import java.util.List;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.task_manager.dto.ProjectRequest;
+import com.example.task_manager.dto.ProjectResponse;
+import com.example.task_manager.model.Project;
+import com.example.task_manager.repository.ProjectRepository;
+import com.example.task_manager.service.ProjectService;
+
+@RestController
+@RequestMapping("/projects")
+public class ProjectController {
+
+    private final ProjectService projectService;
+
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ProjectResponse createProject(ProjectRequest request) {
+
+        Project project = projectService.createProject(request);
+
+        return new ProjectResponse(project.getId(), project.getName(), project.getDirection().getId());
+    }
+
+    @GetMapping
+    public List<ProjectResponse> getAllProjects() {
+
+        return projectService.getAllProjects()
+                .stream()
+                .map(p -> new ProjectResponse(
+                        p.getId(),
+                        p.getName(),
+                        p.getDirection().getId()
+                ))
+                .toList();
+    }
+
+    
+}
