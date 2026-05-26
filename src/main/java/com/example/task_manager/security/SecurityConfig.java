af389458e6a807b90a110de92b89507c5b75bf47
Kirill <matveeffew@gmail.com>
2026-05-26 15:39:37 +0300

    убран второй конфиг

--- src/main/java/com/example/task_manager/security/SecurityConfig.java
+++ src/main/java/com/example/task_manager/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.task_manager.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
@@ -32,27 +33,16 @@ public class SecurityConfig {
     }
 
     @Bean
-    @Profile("dev")
-    public SecurityFilterChain devSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/h2-console/**").permitAll()
-                .anyRequest().permitAll()
-            )
-            .csrf(csrf -> csrf.disable())
-            .headers(headers -> headers.frameOptions().sameOrigin());
-        return http.build();
-
-    }
-
-    @Bean
-    @Profile("prod")
-    public SecurityFilterChain prodSecurityFilterChain( HttpSecurity http) throws Exception {
+    public SecurityFilterChain SecurityFilterChain( HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/users/register").permitAll()
                 .requestMatchers("/info/**").permitAll()
                 .requestMatchers("/admin/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/directions/**").hasAnyRole("USER", "ADMIN")
+                .requestMatchers("/directions/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/projects/**").hasAnyRole("USER", "ADMIN")
+                .requestMatchers("/projects/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
             )
             .httpBasic(Customizer.withDefaults())
