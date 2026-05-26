eb021da99d726c3b8d0d4aa15981a1b91a47683f
Kirill <matveeffew@gmail.com>
2026-05-26 15:41:27 +0300

    тесты контроллеров и сервисов новых сущностей

---
+++ src/test/java/com/example/task_manager/service/DirectionServiceTest.java
@@ -0,0 +1,132 @@
+package com.example.task_manager.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.task_manager.dto.DirectionRequest;
+import com.example.task_manager.dto.DirectionResponse;
+import com.example.task_manager.model.Direction;
+import com.example.task_manager.repository.DirectionRepository;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("\n =======DirectionService Unit Tests======= \n")
+public class DirectionServiceTest {
+
+    @Mock
+    private DirectionRepository directionRepository;
+
+    @InjectMocks
+    private DirectionService directionService;
+
+    @Test
+    @DisplayName("createDirection() -> создает направление")
+    public void createDirection_CreateNewDirection() {
+
+        DirectionRequest request = new DirectionRequest("new dir");
+
+        Direction savedDirection = new Direction();
+        savedDirection.setId(1L);
+        savedDirection.setName("new dir");
+
+        when(directionRepository.save(any(Direction.class)))
+                .thenReturn(savedDirection);
+
+        Direction result = directionService.createDirection(request);
+
+        assertEquals("new dir", result.getName());
+        assertEquals(1L, result.getId());
+
+        verify(directionRepository).save(any(Direction.class));
+    }
+
+    @Test
+    @DisplayName("createDirection() -> создает направление")
+    public void createDirectionWithEmptyTitle_ReturnsException() {
+
+        DirectionRequest request = new DirectionRequest("");
+
+        IllegalArgumentException ex = assertThrows(
+            IllegalArgumentException.class,
+            () -> directionService.createDirection(request)
+        );
+
+        assertEquals("Имя направления не может быть пустым", ex.getMessage());
+
+    }
+
+    @Test
+    @DisplayName("createDirection() -> создает направление")
+    public void createDirectionWithNullTitle_ReturnsException() {
+
+        DirectionRequest request = new DirectionRequest(null);
+
+        IllegalArgumentException ex = assertThrows(
+            IllegalArgumentException.class,
+            () -> directionService.createDirection(request)
+        );
+
+        assertEquals("Имя направления не может быть пустым", ex.getMessage());
+
+    }
+    
+
+    @Test
+    @DisplayName("createDirection() -> дубликат имени вызывает исключение")
+    void createDirectionWithDuplicateName_ThrowsException() {
+
+        DirectionRequest request = new DirectionRequest("same name");
+
+        when(directionRepository.existsByName("same name"))
+                .thenReturn(true);
+
+        IllegalArgumentException ex = assertThrows(
+                IllegalArgumentException.class,
+                () -> directionService.createDirection(request)
+        );
+
+        assertEquals("Направление с таким названием уже существует.", ex.getMessage());
+
+        verify(directionRepository).existsByName("same name");
+        verify(directionRepository, never()).save(any());
+    }
+
+    @Test
+    @DisplayName("getAllDirections() -> вызывает все направления")
+    public void getAllDirections_ReturnsAllDirections() {
+
+        Direction savedDirection1 = new Direction();
+        savedDirection1.setId(1L);
+        savedDirection1.setName("new dir");
+
+        Direction savedDirection2 = new Direction();
+        savedDirection2.setId(2L);
+        savedDirection2.setName("new dir");
+
+        List<Direction> dirs = new ArrayList<>();
+
+        dirs.add(savedDirection1);
+        dirs.add(savedDirection2);
+
+        when(directionRepository.findAll()).thenReturn(dirs);
+
+        List<Direction> result = directionService.getAllDirections();
+
+        assertEquals(2, result.size());
+
+    }
+}
---
+++ src/test/java/com/example/task_manager/service/ProjectServiceTest.java
@@ -0,0 +1,127 @@
+package com.example.task_manager.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.example.task_manager.dto.ProjectRequest;
+import com.example.task_manager.model.Direction;
+import com.example.task_manager.model.Project;
+import com.example.task_manager.repository.DirectionRepository;
+import com.example.task_manager.repository.ProjectRepository;
+import com.example.task_manager.security.UserSecurityService;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("\n =======ProjectService Unit Tests======= \n")
+public class ProjectServiceTest {
+
+    @Mock 
+    private ProjectRepository projectRepository;
+
+    @Mock
+    private DirectionRepository directionRepository;
+
+    @InjectMocks
+    private ProjectService projectService;
+
+    @Test 
+    public void createProject_RetunrsCreatedProject() throws Exception{
+        
+        ProjectRequest request = new ProjectRequest("Project 1", 1L);
+
+        Direction direction = new Direction();
+        direction.setId(1L);
+
+        Project savedProject = new Project();
+        savedProject.setId(1L);
+        savedProject.setName("Project 1");
+        savedProject.setDirection(direction);
+
+        when(directionRepository.findById(1L)).thenReturn(Optional.of(direction));
+
+        when(projectRepository.existsByName("Project 1")).thenReturn(false);
+
+        when(projectRepository.save(any(Project.class))).thenReturn(savedProject);
+
+        Project result = projectService.createProject(request);
+
+        assertEquals("Project 1", result.getName());
+        assertEquals(1L, result.getDirection().getId());
+    } 
+
+    @Test
+    void createProject_emptyName_shouldThrowException() {
+
+        ProjectRequest request = new ProjectRequest(" ", 1L);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> projectService.createProject(request));
+    }
+
+    @Test
+    void createProject_duplicate_shouldThrowException() {
+
+        ProjectRequest request = new ProjectRequest("Project 1", 1L);
+
+        when(projectRepository.existsByName("Project 1")).thenReturn(true);
+
+        assertThrows(IllegalArgumentException.class,
+                () -> projectService.createProject(request));
+    }
+
+    @Test
+    void createProject_directionNotFound_shouldThrowException() {
+
+        ProjectRequest request = new ProjectRequest("Project 1", 1L);
+
+        when(projectRepository.existsByName(anyString()))
+                .thenReturn(false);
+
+        when(directionRepository.findById(1L))
+                .thenReturn(Optional.empty());
+
+        assertThrows(IllegalArgumentException.class,
+                () -> projectService.createProject(request));
+    }
+
+    @Test
+    void getAllProjects_success() {
+
+        Direction direction = new Direction();
+        direction.setId(1L);
+
+        Project p1 = new Project();
+        p1.setId(1L);
+        p1.setName("P1");
+        p1.setDirection(direction);
+
+        Project p2 = new Project();
+        p2.setId(2L);
+        p2.setName("P2");
+        p2.setDirection(direction);
+
+        when(projectRepository.findAll())
+                .thenReturn(List.of(p1, p2));
+
+        List<Project> result = projectService.getAllProjects();
+
+        assertEquals(2, result.size());
+    }
+
+}
