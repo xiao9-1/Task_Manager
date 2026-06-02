b5f343b43d8875288eee27e4e0671b173a57e0d9
Kirill <matveeffew@gmail.com>
2026-06-02 14:56:48 +0300

    uses Db srcs from main

--- src/test/java/com/example/task_manager/integration/AbstractIntegrationTest.java
+++ src/test/java/com/example/task_manager/integration/AbstractIntegrationTest.java
@@ -6,6 +6,7 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.MountableFile;
 
 @Testcontainers
 @SpringBootTest
@@ -18,7 +19,22 @@ public abstract class AbstractIntegrationTest {
                     .withDatabaseName("taskmanager-test")
                     .withUsername("postgres")
                     .withPassword("postgres")
-                    .withInitScript("db/src.sql"
+                    //.withInitScript("db/src.sql"
+                    .withCopyFileToContainer(
+                            MountableFile.forHostPath("docker/db/1_create_ddl.sql"),
+                            "/docker-entrypoint-initdb.d/1_create_ddl.sql"
+                    )
+                    .withCopyFileToContainer(
+                            MountableFile.forHostPath("docker/db/3_alter_table_v0_0_2.sql"),
+                            "/docker-entrypoint-initdb.d/3_alter_table_v0_0_2.sql"
+                    )
+                    .withCopyFileToContainer(
+                            MountableFile.forHostPath("docker/db/4_alter_table_tasks_v0_0_4.sql"),
+                            "/docker-entrypoint-initdb.d/4_alter_table_tasks_v0_0_4.sql"
+                    )
+                    .withCopyFileToContainer(
+                            MountableFile.forHostPath("docker/db/5_create_table_v_0_0_4.sql"),
+                            "/docker-entrypoint-initdb.d/5_create_table_v_0_0_4.sql"
                     );
 
     @DynamicPropertySource
