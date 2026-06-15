7d26b7810f2930c4810c5730750244c7d8c5f4ba
Kirill <matveeffew@gmail.com>
2026-06-15 12:20:15 +0300

    Новый эндпоинт /admin/tasks для Админ-формата задач

--- src/main/java/com/example/task_manager/controller/AdminController.java
+++ src/main/java/com/example/task_manager/controller/AdminController.java
@@ -3,6 +3,12 @@ package com.example.task_manager.controller;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.example.task_manager.dto.AdminTaskResponse;
+import com.example.task_manager.mapper.TaskMapper;
+import com.example.task_manager.model.Task;
+import com.example.task_manager.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,10 +24,14 @@ import com.example.task_manager.service.TaskService;
 @RequestMapping("/admin")
 public class AdminController {
 
+    private static final Logger log = LoggerFactory.getLogger(AdminController.class);
+
     private final TaskService taskService;
-    
-    public AdminController(TaskService taskService) {
+    private final TaskMapper taskMapper;
+
+    public AdminController(TaskService taskService, TaskMapper taskMapper) {
         this.taskService = taskService;
+        this.taskMapper = taskMapper;
     }
 
     @GetMapping("/report/tasks")
@@ -30,6 +40,8 @@ public class AdminController {
             @AuthenticationPrincipal CustomUserDetails user)
 
         {
+
+        log.info("GET/ admin/report/tasks - userId: {}, role: {}", userId, user.getRole());
         return taskService.getReport(userId, user.getRole());
     }
 
@@ -40,6 +52,7 @@ public class AdminController {
             @AuthenticationPrincipal CustomUserDetails user
     ) {
 
+        log.info("GET/ admin/report/tasks-per-hour/utc - userId: {}, role: {}", user.getId(), user.getRole());
         return taskService.getTasksPerHourStatsUtc(
                 from,
                 to,
@@ -54,6 +67,7 @@ public class AdminController {
             @AuthenticationPrincipal CustomUserDetails user
     ) {
 
+        log.info("GET/ admin/report/tasks-per-hour/local - userId: {}, role: {}", user.getId(), user.getRole());
         return taskService.getTasksPerHourStatsLocal(
                 from,
                 to,
@@ -62,6 +76,17 @@ public class AdminController {
         );
     }
 
-    
-    
+    @GetMapping("/tasks")
+    public List<AdminTaskResponse> getAllUsersTask(@AuthenticationPrincipal CustomUserDetails user) {
+        List <Task> tasks = taskService.getAllTaskForAdmin(user.getRole());
+
+        log.info("GET/ admin/tasks - userId: {}, role: {}", user.getId(), user.getRole());
+        log.info("Задач возвращено: {}", tasks.size());
+
+        return tasks
+                .stream()
+                .map(task -> taskMapper.toAdminTaskResponse(task, user.getTimeZone()))
+                .toList();
+
+    }
 }
